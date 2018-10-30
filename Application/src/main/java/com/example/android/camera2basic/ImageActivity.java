89bda449a80277bb8c6c6bb8ba0518666fb12a1f
AlexPetrusca <alexandru.petrusca@yahoo.com>
2018-10-30 16:02:58 -0700

    first commit

---
+++ Application/src/main/java/com/example/android/camera2basic/ImageActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.camera2basic;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class ImageActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        imageView = findViewById(R.id.imageView);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String info = data.getExtras().get("fileName").toString();
+        Bitmap bmImg = BitmapFactory.decodeFile(info);
+        imageView.setImageBitmap(bmImg);
+    }
+}
