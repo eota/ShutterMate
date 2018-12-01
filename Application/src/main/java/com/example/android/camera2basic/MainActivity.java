5ed4e5519c2267936849a5778ffe477a809dcb7c
eota <eota@ucsc.edu>
2018-11-30 18:39:37 -0800

    GUI improvements to Activity Main + small changes

--- Application/src/main/java/com/example/android/camera2basic/MainActivity.java
+++ Application/src/main/java/com/example/android/camera2basic/MainActivity.java
@@ -6,17 +6,32 @@ import android.graphics.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.TapUserMessageTxt);
+        startFadeInAnimation(text);
+
     }
     public void camera(View view){
         Intent i = new Intent(getApplicationContext(), CameraActivity.class);
         startActivity(i);
-
     }
+
+    public void startFadeInAnimation(View view) {
+        TextView t = text;
+        Animation startAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in_animation);
+        t.startAnimation(startAnimation);
+    }
+
 }
